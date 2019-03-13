2bfc9c02dc5bb60c8d4b5c8f9afb7ed42b302c02
Noah Fleischmann <noahfleischmann21@gmail.com>
2019-03-13 15:19:30 +0100

    Add mongodb provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@
 	config.vm.define "web" do |web|
 		web.vm.box = "ubuntu/xenial64"
 		web.vm.hostname = "nodejs"
-		web.vm.network "private_network", ip:"192.168.55.100"
+		web.vm.network "private_network", ip: "192.168.55.100"
 		web.vm.network "forwarded_port", guest:80, host:8080, auto_correct: true
 		web.vm.provider "virtualbox" do |vb|
 			vb.memory = "1024"  
--- db.sh
+++ db.sh
@@ -4,8 +4,18 @@
 #
 
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-sudo apt-key update -y
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update -y
-sudo apt-get install -y --allow-unauthenticated mongodb-org
+echo -----------------------------------------------------------------
+echo -                           MongoDB                             -
+echo -----------------------------------------------------------------
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+mkdir -p /data/db
+chmod -R 777 /data/db
+
+sed -i -e 's/127.0.0.1/192.168.55.101/g' /etc/mongod.conf
+
+service mongod start
